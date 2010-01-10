7e0c870183f05816851dd89bcff7109f84bd7366
sandor.norbert <sandor.norbert@erinors.com>
2010-01-10 18:15:35 +0000

    create a test conforming to the Datanucleus forum requirements at http://www.datanucleus.org/project/problem_jpa_testcase.html

---
+++ jvminside-experimenting-with-datanucleus/src/main/java/com/jvminside/blog/experimenting_with_datanucleus/DetachedEntityTestForDatanucleusForum.java
@@ -0,0 +1,134 @@
+package com.jvminside.blog.experimenting_with_datanucleus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import junit.framework.Assert;
+
+import org.datanucleus.util.NucleusLogger;
+
+public class DetachedEntityTestForDatanucleusForum
+{
+    private static EntityManagerFactory emf;
+
+    private static EntityManager em;
+
+    public static void main(String[] args)
+    {
+        Map<String, String> props = new HashMap<String, String>();
+        props.put("javax.persistence.jdbc.driver", "org.hsqldb.jdbcDriver");
+        props.put("javax.persistence.jdbc.url", "jdbc:hsqldb:mem:db");
+        props.put("javax.persistence.jdbc.user", "sa");
+        props.put("javax.persistence.jdbc.password", "");
+        props.put("datanucleus.autoCreateSchema", "true");
+        props.put("datanucleus.jpa.addClassTransformer", "false");
+        props.put("datanucleus.storeManagerType", "rdbms");
+
+        emf = Persistence.createEntityManagerFactory("forDatanucleusForum", props);
+
+        //
+        // Populate database
+        //
+
+        final Parent[] detached = new Parent[1];
+        final int originalVersion;
+
+        executeTransactionally(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Parent parent = (Parent) new Parent().presetId(100);
+
+                parent.getChildren().add((Child) new Child(0).presetId(101));
+                parent.getChildren().add((Child) new Child(1).presetId(102));
+                parent.getChildren().add((Child) new Child(2).presetId(103));
+                getEntityManager().persist(parent);
+
+                getEntityManager().flush();
+
+                detached[0] = parent;
+            }
+        });
+
+        originalVersion = detached[0].getVersion();
+
+        final Child deleted = detached[0].getChildren().iterator().next();
+        detached[0].getChildren().remove(deleted);
+
+        //
+        // Merge unmodified
+        //
+
+        executeTransactionally(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Parent merged = getEntityManager().merge(detached[0]);
+                Assert.assertEquals(2, merged.getChildren().size());
+            }
+        });
+
+        //
+        // Check result
+        //
+
+        executeTransactionally(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Assert.assertEquals("The detached instance was probably persisted as a new instance: ", 1,
+                        ((Number) getEntityManager().createQuery("SELECT COUNT(p) FROM Parent p").getSingleResult())
+                                .intValue());
+
+                Parent parent = getEntityManager().find(Parent.class, 100);
+                Assert.assertEquals("Version number wasn't incremented as expected: ", originalVersion + 1, parent
+                        .getVersion());
+                Assert.assertEquals("Parent should have 2 children because one of them was removed: ", 2, parent
+                        .getChildren().size());
+            }
+        });
+
+        emf.close();
+    }
+
+    private static void executeTransactionally(Runnable runnable)
+    {
+        em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try
+        {
+            tx.begin();
+
+            runnable.run();
+
+            tx.commit();
+        }
+        catch (Exception e)
+        {
+            NucleusLogger.GENERAL.info(">> Exception thrown persisting objects : " + e.getMessage());
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+
+            em.close();
+            em = null;
+        }
+    }
+
+    private static EntityManager getEntityManager()
+    {
+        return em;
+    }
+}
